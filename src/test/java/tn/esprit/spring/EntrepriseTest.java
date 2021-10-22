b09262ef2a7bc5547a9e1cac6461137777a081c8
Saidi Ahmed <ahmed.saidi@esprit.tn>
2021-10-22 20:59:01 +0100

    CLEAN PROJECT :pill:

---
+++ src/test/java/tn/esprit/spring/EntrepriseTest.java
@@ -0,0 +1,33 @@
+package tn.esprit.spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.EntrepriseRepository;
+import tn.esprit.spring.services.IEntrepriseService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseTest {
+	
+    @Autowired
+    private IEntrepriseService serviceEntreprise;
+
+    @Autowired
+    private EntrepriseRepository repoEntreprise;
+    
+    @Test
+    public void testAjouterEntreprise(){
+        Entreprise ent= new Entreprise("SaidiJam Mat","Agriculture");
+        int id=serviceEntreprise.ajouterEntreprise((ent));
+
+        Assert.assertNotNull(serviceEntreprise.getEntrepriseById(id));
+    }
+    
+
+}
