88d9b3932523e6c1f868f3e17b4cc822a024dcfa
SaidiAhmed11 <ahmed.saidi@esprit.tn>
2021-10-31 17:50:02 +0100

    Fix All Sonar Bugs in EntrepriseService :100:

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,8 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 
+	String errorText = "Erreur:";
+
 	public int ajouterEntreprise(Entreprise entreprise) {
 		logger.info("START ajouterEntreprise ");
 
@@ -36,7 +39,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 					+ " ajoutée avec succé");
 
 		} catch (Exception e) {
-			logger.error("Erreur:" + e);
+			logger.error(errorText + e);
 		}
 		logger.info("END ajouterEntreprise ");
 
@@ -56,7 +59,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 			logger.debug("l'dep: " + dep.getName() + " de l'id: " + dep.getId() + " ajoutée avec succé");
 
 		} catch (Exception e) {
-			logger.error("Erreur:" + e);
+			logger.error(errorText + e);
 		}
 		logger.info("END ajouterdep ");
 
@@ -69,30 +72,42 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 		try {
 			logger.debug("init dep" + dep);
 
-			logger.trace("début: récuperer entrep by ID");
-			Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-			logger.trace("fin: récuperer entrep by ID");
+			Optional<Entreprise> e = entrepriseRepoistory.findById(entrepriseId);
+			Optional<Departement> d = deptRepoistory.findById(depId);
+			logger.trace("Début Test : verifier l'existence du l'entreprise et du Departement");
 
-			logger.trace("début: récuperer dep by ID");
-			Departement depManagedEntity = deptRepoistory.findById(depId).get();
-			logger.trace("début: récuperer dep by ID");
+			if (e.isPresent() && d.isPresent()) {
 
-			logger.trace("début: affectation d'entrep -> departement");
-			depManagedEntity.setEntreprise(entrepriseManagedEntity);
-			deptRepoistory.save(depManagedEntity);
-			logger.trace("fin: affectation d'entrep -> departement");
+				logger.trace("début: récuperer entrep by ID");
+				Entreprise entrepriseManagedEntity = e.get();
+				logger.trace("fin: récuperer entrep by ID");
 
-			dep = depManagedEntity;
+				logger.trace("début: récuperer dep by ID");
+				Departement depManagedEntity = d.get();
+				logger.trace("début: récuperer dep by ID");
 
-			logger.debug("new Dep" + dep);
+				logger.trace("début: affectation d'entrep -> departement");
+				depManagedEntity.setEntreprise(entrepriseManagedEntity);
+				deptRepoistory.save(depManagedEntity);
+				logger.trace("fin: affectation d'entrep -> departement");
 
-			logger.debug("Entrep: " + depManagedEntity.getEntreprise().getId() + "-"
-					+ depManagedEntity.getEntreprise().getName() + "affecté au department:" + depManagedEntity.getName()
-					+ "-" + depManagedEntity.getId());
+				dep = depManagedEntity;
+
+				logger.debug("new Dep" + dep);
+
+				logger.debug("Entrep: " + depManagedEntity.getEntreprise().getId() + "-"
+						+ depManagedEntity.getEntreprise().getName() + "affecté au department:"
+						+ depManagedEntity.getName() + "-" + depManagedEntity.getId());
+				logger.trace("FIN Test : verifier l'existence du l'entreprise et du Departement");
+
+			}
+		 else {
+			logger.trace("Entrep ou Dep n'exitse pas");
+			logger.trace("FIN Test : verifier l'existence du l'entreprise et du Departement");
+		}
 
 		} catch (Exception e) {
-			logger.error("Erreur: " + e);
-
+			logger.error(errorText + e);
 		}
 
 		logger.info("END affecterDepartementAEntreprise ");
@@ -100,41 +115,53 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	}
 
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
-		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 		List<String> depNames = new ArrayList<>();
-		for (Departement dep : entrepriseManagedEntity.getDepartements()) {
-			depNames.add(dep.getName());
-		}
+		Optional<Entreprise> e = entrepriseRepoistory.findById(entrepriseId);
+		if (e.isPresent()) {
+			Entreprise entrepriseManagedEntity = e.get();
 
+			for (Departement dep : entrepriseManagedEntity.getDepartements()) {
+				depNames.add(dep.getName());
+			}
+
+			return depNames;
+		}
 		return depNames;
+
 	}
 
 	@Transactional
 	public int deleteEntrepriseById(int entrepriseId) {
 		logger.info("START deleteEntrepriseById ");
+		Optional<Entreprise> e = entrepriseRepoistory.findById(entrepriseId);
+
 		try {
-			logger.trace("Début Test : verifier l'existence du l'entrep");
-			if (entrepriseRepoistory.findById(entrepriseId).isPresent()) {
-				logger.debug("Entrep exitse:" + entrepriseRepoistory.findById(entrepriseId).get().getId());
+
+			logger.trace("Début Test : verifier l'existence du l'entreprise");
+			if (e.isPresent()) {
+				
+				logger.debug("Entrep exitse:" + e.get().getId());
 
 				logger.trace("débbut suppression");
-				entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());
+				entrepriseRepoistory.delete(e.get());
 				logger.trace("fin suppression");
-				logger.trace("FIN Test : verifier l'existence du l'entrep");
+				logger.trace("FIN Test : verifier l'existence du l'entreprise");
+				
 				return 1;
 			} else {
-
 				logger.trace("Entrep n'exitse pas");
 				logger.trace("FIN Test : verifier l'existence du l'entrep");
 				return -1;
 			}
 
-		} catch (Exception e) {
-			logger.error("Erreur: " + e);
+		} catch (Exception err) {
+			logger.error(errorText + err);
 
 		}
-
-		logger.debug("Entrep supprimée:" + entrepriseRepoistory.findById(entrepriseId).get().getId());
+		if(e.isPresent()) {
+			logger.debug("Entrep supprimée:" + e.get().getId());
+		}
+	
 		logger.info("END deleteEntrepriseById ");
 
 		return 0;
@@ -144,13 +171,16 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Transactional
 	public int deleteDepartementById(int depId) {
 		logger.info("START deleteDepartementById ");
+		Optional<Departement> d = deptRepoistory.findById(depId);
+
 		try {
-			logger.trace("Début Test : verifier l'existence du l'entrep");
-			if (deptRepoistory.findById(depId).isPresent()) {
-				logger.debug("Departement exitse:" + deptRepoistory.findById(depId).get().getId());
+			logger.trace("Début Test : verifier l'existence du lDepartement");
+
+			if (d.isPresent()) {
+				logger.debug("Departement exitse:" + d.get().getId());
 
 				logger.trace("débbut suppression");
-				deptRepoistory.delete(deptRepoistory.findById(depId).get());
+				deptRepoistory.delete(d.get());
 				logger.trace("fin suppression");
 				logger.trace("FIN Test : verifier l'existence du Departement");
 				return 1;
@@ -162,11 +192,12 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 			}
 
 		} catch (Exception e) {
-			logger.error("Erreur: " + e);
+			logger.error(errorText+ e);
 
 		}
-
-		logger.debug("Departement suprimée:" + deptRepoistory.findById(depId).get().getId());
+		if(d.isPresent()) {
+			logger.debug("Departement suprimée:" + d.get().getId());
+		}
 		logger.info("END deleteDepartementById ");
 		return 0;
 
@@ -175,19 +206,20 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	public Entreprise getEntrepriseById(int entrepriseId) {
 		logger.info("START getEntrepriseById ");
 		try {
+			Optional<Entreprise> e = entrepriseRepoistory.findById(entrepriseId);
 			logger.trace("Début Test : verifier l'existence du l'entrep");
-			if (entrepriseRepoistory.findById(entrepriseId).isPresent()) {
-				
-				logger.debug("Entreprise exitse:" + entrepriseRepoistory.findById(entrepriseId).get().getId());
-				
+			if (e.isPresent()) {
+
+				logger.debug("Entreprise exitse:" + e.get().getId());
+
 				logger.trace("débbut Get");
-				return entrepriseRepoistory.findById(entrepriseId).get();
+				return e.get();
 			}
-			logger.trace("fin Get");			
+			logger.trace("fin Get");
 			logger.trace("FIN Test : verifier l'existence du l'entrep");
 		} catch (Exception e) {
 			logger.error("Erreur: " + e);
-			
+
 		}
 		logger.info("END getEntrepriseById ");
 
@@ -197,19 +229,21 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	public Departement getDepartementById(int departementId) {
 		logger.info("START getDepartementById ");
 		try {
+			Optional<Departement> d = deptRepoistory.findById(departementId);
+
 			logger.trace("Début Test : verifier l'existence du departement");
-			if (deptRepoistory.findById(departementId).isPresent()) {
-				
-				logger.debug("Entreprise exitse:" + deptRepoistory.findById(departementId).get().getId());
-				
+			if (d.isPresent()) {
+
+				logger.debug("Entreprise exitse:" + d.get().getId());
+
 				logger.trace("débbut Get");
-				return deptRepoistory.findById(departementId).get();
+				return d.get();
 			}
-			logger.trace("fin Get");			
+			logger.trace("fin Get");
 			logger.trace("FIN Test : verifier l'existence du departement");
 		} catch (Exception e) {
-			logger.error("Erreur: " + e);
-			
+			logger.error(errorText+ e);
+
 		}
 		logger.info("END getDepartementById ");
 
