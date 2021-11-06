a5de8ba112605568f7e201f881370b5530092d24
SaidiAhmed11 <ahmed.saidi@esprit.tn>
2021-11-06 20:35:34 +0100

    Add New service functions + tests :100:

--- src/test/java/tn/esprit/spring/EntrepriseTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseTest.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +19,6 @@ public class EntrepriseTest {
 
 	@Autowired
 	private IEntrepriseService serviceEntreprise;
-
 
 	@Test
 	public void testAjouterEntreprise() {
@@ -41,20 +42,20 @@ public class EntrepriseTest {
 		Departement dep = new Departement("Web");
 		int idDep = serviceEntreprise.ajouterDepartement(dep);
 
-		int idEntrepDep=serviceEntreprise.affecterDepartementAEntreprise(idDep, idEntrep);
-		Assert.assertEquals(idEntrepDep,idEntrep);
+		int idEntrepDep = serviceEntreprise.affecterDepartementAEntreprise(idDep, idEntrep);
+		Assert.assertEquals(idEntrepDep, idEntrep);
 
 	}
 
 	@Test
 	public void testdeleteEntrepriseById() {
-		
+
 		Entreprise ent = new Entreprise("Soc", "DEV");
 		int id = serviceEntreprise.ajouterEntreprise(ent);
-		
+
 		int value = serviceEntreprise.deleteEntrepriseById(id);
 		Assert.assertEquals(1, value);
-		
+
 		int WrongValue = serviceEntreprise.deleteEntrepriseById(1812132);
 		Assert.assertEquals(WrongValue, -1);
 
@@ -64,10 +65,10 @@ public class EntrepriseTest {
 	public void testdeleteDepartementById() {
 		Departement dep = new Departement("Info");
 		int id = serviceEntreprise.ajouterDepartement(dep);
-		
+
 		int value = serviceEntreprise.deleteDepartementById(id);
 		Assert.assertEquals(1, value);
-		
+
 		int WrongValue = serviceEntreprise.deleteDepartementById(1812132);
 		Assert.assertEquals(WrongValue, -1);
 
@@ -78,7 +79,7 @@ public class EntrepriseTest {
 
 		Entreprise ent = new Entreprise("Soc", "DEV");
 		int id = serviceEntreprise.ajouterEntreprise(ent);
-		
+
 		Entreprise e1 = serviceEntreprise.getEntrepriseById(id);
 		Assert.assertNotNull(e1);
 
@@ -90,13 +91,94 @@ public class EntrepriseTest {
 	public void testgetDepartementById() {
 		Departement dep = new Departement("Info");
 		int id = serviceEntreprise.ajouterDepartement(dep);
-		
-		
+
 		Departement d1 = serviceEntreprise.getDepartementById(id);
 		Assert.assertNotNull(d1);
 
 		Departement d2 = serviceEntreprise.getDepartementById(213232);
 		Assert.assertNull(d2);
 	};
+
+	@Test
+	public void testretrieveAllEntreprises() {
+
+		boolean test = false;
+
+		Entreprise ent1 = new Entreprise("Soc", "DEV");
+		Entreprise ent2 = new Entreprise("info", "web");
+
+		serviceEntreprise.ajouterEntreprise(ent1);
+		serviceEntreprise.ajouterEntreprise(ent2);
+
+		List<Entreprise> entreprises = serviceEntreprise.retrieveAllEntreprises();
+
+		if (entreprises.size() > 2) {
+			test = true;
+		}
+		Assert.assertTrue(test);
+	}
+
+	@Test
+	public void testretrieveAllDepartements() {
+
+		boolean test = false;
+
+		Departement dep = new Departement("WEB");
+		Departement dep1 = new Departement("DS");
+
+		serviceEntreprise.ajouterDepartement(dep);
+		serviceEntreprise.ajouterDepartement(dep1);
+
+		List<Departement> departementts = serviceEntreprise.retrieveAllDepartements();
+
+		if (departementts.size() > 2) {
+			test = true;
+		}
+		Assert.assertTrue(test);
+	}
+
+	@Test
+	public void testupdateDepartement() {
+
+		Departement dep1 = new Departement("DS");
+
+		int id = serviceEntreprise.ajouterDepartement(dep1);
+
+		Departement Newdep = new Departement("WEB");
+
+		Departement updatedDep = serviceEntreprise.updateDepartement(id, Newdep);
+
+		Assert.assertEquals(Newdep.getName(), updatedDep.getName());
+
+	}
+
+	@Test
+	public void testupdateEntreprise() {
+
+		Entreprise ent = new Entreprise("info", "web");
+
+		int id = serviceEntreprise.ajouterEntreprise(ent);
+
+		Entreprise NewEntrep = new Entreprise("ESPRIT", "Info");
+
+		Entreprise updatedEntrep = serviceEntreprise.updateEntreprise(id, NewEntrep);
+
+		Assert.assertEquals(NewEntrep.getName(), updatedEntrep.getName());
+		Assert.assertEquals(NewEntrep.getRaisonSocial(), updatedEntrep.getRaisonSocial());
+	}
+
+	@Test
+	public void testdeleteAllEntreprises() {
+		int value = serviceEntreprise.deleteAllEntreprises();
+		Assert.assertEquals(1, value);
+
+	}
+
+	@Test
+	public void testdeleteAllDepartements() {
+		int value = serviceEntreprise.deleteAllDepartements();
+		Assert.assertEquals(1, value);
+
+	}
 
 }
